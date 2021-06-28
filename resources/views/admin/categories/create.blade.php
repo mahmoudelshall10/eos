8628923738a688293358bf166379c18a2d846051
elshal <mahmoudelshall10@outlook.com>
2021-06-28 16:51:29 +0200

    28-jun-21-5

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -9,15 +9,7 @@
             <div class="col-lg-12">
                 <div class="breadcome-list map-mg-t-40-gl shadow-reset">
                     <div class="row">
-                        <div class="col-lg-6">
-                            <div class="breadcome-heading">
-                                <form role="search" class="">
-                                    <input type="text" placeholder="Search..." class="form-control">
-                                    <a href=""><i class="fa fa-search"></i></a>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
+                        <div class="col-lg-12">
                             <ul class="breadcome-menu">
                                 @can('admin.dashboards.index')
                                 <li>
